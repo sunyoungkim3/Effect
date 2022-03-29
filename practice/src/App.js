b01fda6ba8044c7290404ebcec1c33a7e89f6f7c
sunyoungkim3 <sykim3@seegene.com>
2022-03-29 21:00:01 +0900

    useEffect: state 변화에 상관없이 한번만 redering 하는 기능

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,11 +1,19 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import {useState, useEffect} from "react";
 
 function App() {
-  return (
-  <div><h1 className={styles.title}>Welcome Back!</h1>
-  <Button text="Confirm"/>
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev +1);
+
+  console.log("I run all the time");
+
+  useEffect(() => {
+    console.log("CALL THE API")
+  }, []);
   
+  return (
+  <div>
+  <h1>{counter}</h1>
+  <button onClick={onClick}>click me</button>
   </div>
   );
 }
